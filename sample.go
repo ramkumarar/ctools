3a6e0719b5c20105f4cd21a4880f2b22ce613138
ramkumarar <ramkumara.ar@gmail.com>
2020-11-09 22:20:04 +0530

    Create sample.go

---
+++ sample.go
@@ -0,0 +1,22 @@
+n := map[string]string{"name": "ram sdlhfsfh sdlhfshdfhsdlfh  sdfhslhflshf sfhsdlf slhflshf slhflshfl shlfhslhflshdfhsdlfhosfoahosh fsfh sdh", "age": "42", "gender": "M", "city": "chennai"}
+	m := map[string]string{"name": "kokila sdhshfd sfhlshfd shfshlf shflshdflhslfhlsf sjf;sjfspfjs;df;sjf sjfsjdf ssfshfshdlfhsl flsdfpsdfshkldfhlshdf lshfhsfsf sfamk'fksf'js fsd", "age": "7", "gender": "F", "city": "chennai"}
+
+	changelogs, _ := diff.Diff(n, m)
+
+	var data [][]string
+
+	for _, changelog := range changelogs {
+		var row []string
+
+		row = append(row, strings.Join(changelog.Path, ""))
+		row = append(row, fmt.Sprintf("%s", changelog.From))
+		row = append(row, fmt.Sprintf("%s", changelog.To))
+		data = append(data, row)
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+	table.SetHeader([]string{"Key", "Source", "Destination"})
+	table.AppendBulk(data)
+	table.SetRowLine(true)
+	table.Render()
